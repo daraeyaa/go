c9c1119bd249ae46e2bec5d0c2c76f44cb33aa08
daraeyaa <daraeyaa@naver.com>
2021-09-18 06:35:40 +0900

    v0.6

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.4
+// 난수 추출된 수의 소수 판정 프로그램 v0.6
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -18,7 +18,9 @@ func main() {
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			isPrime = false
+			break // 첫 번째 약수가 발견되면 반복문 즉시 종료
 		}
+		//fmt.Print(i, " ")
 	}
 
 	if isPrime { // 비교연산자 제거
